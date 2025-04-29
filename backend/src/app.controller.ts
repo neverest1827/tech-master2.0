2f6363546e34fa3ec38e875f961c1869bbbd1221
Vitalii <neverest1827@gmail.com>
2025-04-29 15:08:13 +0300

    ✨feat(app): connect BlogService in the controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,24 @@
 import {Controller, Get, Render} from '@nestjs/common';
-import { AppService } from './app.service';
+import {AppService} from './app.service';
+import {BlogService} from "./blog/blog.service";
+import {BlogPost} from "./blog/entities/blog-post.entity";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly blogService: BlogService,
+    ) {}
 
-  @Get()
-  @Render('index')
-  getIndexPage() {
-    return {
-      title: 'Главная страница',
-      env: process.env.NODE_ENV
-    };
-  }
+    @Get()
+    @Render('index')
+    async getIndexPage() {
+        const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
+
+        return {
+            title: 'Главная страница',
+            env: process.env.NODE_ENV,
+            blogPosts: blogPosts,
+        };
+    }
 }
