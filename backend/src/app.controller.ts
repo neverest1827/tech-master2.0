22476163470ebfb5c929ce716a8a7f646ad1f016
Vitalii <neverest1827@gmail.com>
2025-05-12 15:35:55 +0300

    ✨feat(app): get the FAQ list from the service and pass it to the EJS template

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,8 @@ import {BlogService} from "./blog/blog.service";
 import {BlogPost} from "./blog/entities/blog-post.entity";
 import {ReviewService} from "./review/review.service";
 import {Review} from "./review/entities/review.entity";
+import {FaqService} from "./faq/faq.service";
+import {Faq} from "./faq/entities/faq.entity";
 
 @Controller()
 export class AppController {
@@ -11,6 +13,7 @@ export class AppController {
         private readonly appService: AppService,
         private readonly blogService: BlogService,
         private readonly reviewService: ReviewService,
+        private readonly faqService: FaqService,
     ) {}
 
     @Get()
@@ -18,12 +21,14 @@ export class AppController {
     async getIndexPage() {
         const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
         const reviews: Review[] = await this.reviewService.findMany(18);
+        const faqs: Faq[] = await this.faqService.findAll();
 
         return {
             title: 'Главная страница',
             env: process.env.NODE_ENV,
             reviews: reviews,
             blogPosts: blogPosts,
+            faqs: faqs,
             scriptName: 'main',
             styleName: 'main'
         };
