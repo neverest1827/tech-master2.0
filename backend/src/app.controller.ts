06116edf0972fa6a72066db8d9d479a03081d81e
Vitalii <neverest1827@gmail.com>
2025-08-11 14:03:23 +0300

    ✨ feat(app): Add a route for the offer page. Connect the offer and meta services to the application controller.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Render} from '@nestjs/common';
+import {Controller, Get, NotFoundException, Param, Render, Req} from '@nestjs/common';
 import {AppService} from './app.service';
 import {BlogService} from "./blog/blog.service";
 import {BlogPost} from "./blog/entities/blog-post.entity";
@@ -6,6 +6,10 @@ import {ReviewService} from "./review/review.service";
 import {Review} from "./review/entities/review.entity";
 import {FaqService} from "./faq/faq.service";
 import {Faq} from "./faq/entities/faq.entity";
+import {OfferService} from "./offer/offer.service";
+import {Offer} from "./offer/entities/offer.entity";
+import {MetaService} from "./meta/meta.service";
+import {Meta} from "./meta/entities/meta.entity";
 
 @Controller()
 export class AppController {
@@ -14,6 +18,8 @@ export class AppController {
         private readonly blogService: BlogService,
         private readonly reviewService: ReviewService,
         private readonly faqService: FaqService,
+        private readonly offerService: OfferService,
+        private readonly metaService: MetaService,
     ) {}
 
     @Get()
@@ -22,15 +28,16 @@ export class AppController {
         const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
         const reviews: Review[] = await this.reviewService.findMany(18);
         const faqs: Faq[] = await this.faqService.findAll();
+        const meta: Meta = await this.metaService.getMetaById('index');
 
         return {
-            title: 'Главная страница',
             env: process.env.NODE_ENV,
-            reviews: reviews,
-            blogPosts: blogPosts,
-            faqs: faqs,
             scriptName: 'main',
-            styleName: 'main'
+            styleName: 'main',
+            meta,
+            reviews,
+            blogPosts,
+            faqs
         };
     }
 
@@ -38,12 +45,13 @@ export class AppController {
     @Render('reviews')
     async getReviewsPage() {
         const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
+        const meta: Meta = await this.metaService.getMetaById('reviews');
 
         return {
             env: process.env.NODE_ENV,
             scriptName: 'reviews',
             styleName: 'reviews',
-            title: 'Отзывы',
+            meta,
             blogPosts,
         }
     }
@@ -59,13 +67,36 @@ export class AppController {
     @Render('services')
     async getServicesPage() {
         const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
+        const meta: Meta = await this.metaService.getMetaById('services');
 
         return {
             env: process.env.NODE_ENV,
             scriptName: 'services',
             styleName: 'services',
-            title: 'Услуги',
+            meta,
             blogPosts,
         }
+    }
+
+    @Get('/uslugi/*slugPath')
+    @Render('offer')
+    async getServiceEntry(@Param('slugPath') slugPath: string) {
+        const slugParts = slugPath.split('/').filter(Boolean);
+
+        const blogPosts: BlogPost[] = await this.blogService.findMany([1,2]);
+        const reviews: Review[] = await this.reviewService.findMany(18);
+        const offerEntry: Offer | null = await this.offerService.findBySlugPath(slugParts.at(-1));
+
+        console.log(offerEntry);
+
+        return {
+            env: process.env.NODE_ENV,
+            scriptName: 'offer',
+            styleName: 'offer',
+            meta: offerEntry?.meta,
+            content: offerEntry?.content || '',
+            blogPosts,
+            reviews,
+        };
     }
 }
