0cb06bd49f2bd77598c654ff80933637e0712e0d
Vitalii <neverest1827@gmail.com>
2025-08-17 09:31:49 +0300

    ✨ feat(error): Add logic for error pages

---
+++ frontend/src/scripts/error.ts
@@ -0,0 +1,32 @@
+import {
+    addBackLinkToServiceList,
+    addDropdownServiceList,
+    addMenuListener,
+    addNavLinkListeners,
+    createBurgerMenu,
+    deactivateServiceLink
+} from "./header.ts";
+import {addModalListeners} from "./modal.ts";
+
+(() => {
+    // Определяем, является ли ширина экрана 1024px или меньше
+    const isMobile: boolean = window.matchMedia('(max-width: 1024px)').matches;
+
+    // Получаем элемент списка услуг (предполагаем, что он всегда есть)
+    const serviceList: HTMLElement = document.querySelector('.dropdown__services-list')!;
+
+    document.addEventListener("DOMContentLoaded", async () => {
+        if (isMobile) {
+            addBackLinkToServiceList(serviceList);
+            createBurgerMenu();
+        } else {
+            addDropdownServiceList(serviceList);
+            deactivateServiceLink();
+        }
+
+        // Добавляем слушатели событий
+        addMenuListener();
+        addModalListeners();
+        addNavLinkListeners();
+    });
+})();
