1626d0675188738da062c25eaf9349fa1dbc17cf
Vitalii <neverest1827@gmail.com>
2025-08-21 19:39:55 +0300

    ✨ feat(promo): Add a promo resource

---
+++ backend/src/promo/promo.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PromoService } from './promo.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Promo} from "./entities/promo.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Promo])],
+  providers: [PromoService],
+  exports: [PromoService]
+})
+export class PromoModule {}
