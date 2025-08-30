fe501e4dfe6e21efd3294921b5d36f86a9816551
Vitalii <neverest1827@gmail.com>
2025-08-30 18:54:54 +0300

    ✨ feat(entity): Add a link between FAQ and Offer table

--- backend/src/offer/entities/offer.entity.ts
+++ backend/src/offer/entities/offer.entity.ts
@@ -4,9 +4,10 @@ import {
     Column,
     CreateDateColumn,
     UpdateDateColumn,
-    Index, OneToOne, JoinColumn,
+    Index, OneToOne, JoinColumn, OneToMany, ManyToMany,
 } from 'typeorm';
 import {Meta} from "../../meta/entities/meta.entity";
+import {Faq} from "../../faq/entities/faq.entity";
 
 @Entity('offer')
 export class Offer {
@@ -23,6 +24,9 @@ export class Offer {
     @OneToOne(() => Meta, (meta) => meta.offer, { cascade: true })
     @JoinColumn()
     meta: Meta;
+
+    @ManyToMany(() => Faq, (faq) => faq.offer, { cascade: true })
+    faqs: Faq[];
 
     @CreateDateColumn()
     createdAt: Date;
