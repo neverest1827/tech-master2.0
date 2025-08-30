724b78241f0817eedc8eeb2628c2486eebdf1521
Vitalii <neverest1827@gmail.com>
2025-08-30 18:36:26 +0300

    ✨ feat(breadcrumb): Add a module, service, and entity for breadcrumbs

---
+++ backend/src/breadcrumb/breadcrumb.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { BreadcrumbService } from './breadcrumb.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Breadcrumb} from "./entities/breadcrumb.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Breadcrumb])],
+  providers: [BreadcrumbService],
+  exports: [BreadcrumbService],
+})
+export class BreadcrumbModule {}
---
+++ backend/src/breadcrumb/entities/breadcrumb.entity.ts
@@ -0,0 +1,13 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('breadcrumb')
+export class Breadcrumb {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ unique: true, nullable: false })
+    slug: string;
+
+    @Column({ type: 'json' })
+    items: { label: string; href: string }[];
+}
